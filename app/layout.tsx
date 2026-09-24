14c03f024fa726a5448545705d386676beef2341
JavaDeveloper-Sinku <sinkusingh173@gmail.com>
2026-09-24 22:14:21 +0530

    url icon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,12 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "TinyURL - Smart URL Shortener",
   description: "Shorten, track and optimize your links.",
+
+  icons: {
+    icon: "/icons8-link-48.png",
+    shortcut: "/icons8-link-48.png",
+    apple: "/icons8-link-48.png",
+  },
 };
 
 export default function RootLayout({
